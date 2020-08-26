da0ceb7a16cd09c597b7a421d75a0fefe6ea80f1
Margaux <mahaffne@student.42.fr>
2020-08-26 12:27:43 +0200

    Add require fetch module

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 
+const fetch = require("node-fetch")
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
